c00808aab5ec995808f985fb5204f867508330a1
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-21 20:29:11 +0600

    notfound proxy complete

---
+++ src/proxy.js
@@ -0,0 +1,25 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+import {NextResponse} from "next/server";
+import {auth} from "./lib/auth";
+import {headers} from "next/headers";
+
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/my-bookings",
+    "/add-facility",
+    "/manage-facilities",
+    "/facilities/:path",
+  ],
+};
